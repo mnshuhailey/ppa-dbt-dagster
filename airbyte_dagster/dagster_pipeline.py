23ac8f667a6bf7e8b80aaf1b3c17153b9215c9f9
Mohd Noor Shuhailey <mshuhaileyfx@gmail.com>
2024-08-01 15:31:08 +0800

    refactor: update files

--- airbyte_dagster/dagster_pipeline.py
+++ airbyte_dagster/dagster_pipeline.py
@@ -1,6 +1,8 @@
 from dagster import job, repository
 from dagster_dbt import dbt_cli_resource, dbt_run_op
 from dagster_airbyte import airbyte_resource, airbyte_sync_op
+from dagster_postgres import postgres_resource
+from dagster_mssql import mssql_resource
 
 # Airbyte resource configuration
 ppa_airbyte_resource = airbyte_resource.configured(
@@ -11,6 +13,26 @@ ppa_airbyte_resource = airbyte_resource.configured(
         "password": "password"
     }
 )
+
+# Postgres resource configuration
+postgres_db = postgres_resource.configured({
+    "host": "192.168.10.177",
+    "port": 5432,
+    "username": "postgres",
+    "password": "secret123",
+    "database": "postgres_db",
+})
+
+# SQL Server resource configuration
+sqlserver_db = mssql_resource.configured({
+    "server": "10.10.1.199",
+    "port": 1433,
+    "username": "noor.shuhailey",
+    "password": "Lzs.user831",
+    "database": "PPA",
+    "schema": "dbo",
+    "trust_cert": "true"
+})
 
 sync_ppa_asnaf = airbyte_sync_op.configured(
     {"connection_id": "0ea080d7-e172-4a82-8ae5-ecb691b9ec86"},
@@ -23,7 +45,7 @@ dbt = dbt_cli_resource.configured({
     "profiles_dir": "/home/shuhailey/lzs-ppa/ppa-dbt-dagster/ppa_dbt",
 })
 
-@job(resource_defs={"airbyte": ppa_airbyte_resource, "dbt": dbt})
+@job(resource_defs={"airbyte": ppa_airbyte_resource, "dbt": dbt, "postgres_db": postgres_db, "sqlserver_db": sqlserver_db})
 def ppa_data_pipeline():
     #sync_ppa_asnaf()
     dbt_run_op()
--- ppa_dbt/models/staging/insert_into_asnaf_transformed.sql
+++
@@ -1,9 +0,0 @@
-insert into {{ target('airbyte_internal', 'asnaf_transformed') }} (AsnafID, AsnafName, Emel, Age)
-select
-  AsnafID,
-  AsnafName,
-  Emel,
-  Age
-from
-    {{ ref('asnaf_transformed') }}
-
