f1d2d84c645dd741df244965a8d6574797dba968
mnshuhailey <mshuhaileyfx@gmail.com>
2024-08-01 23:08:04 +0800

    progress

--- airbyte_dagster/dagster_pipeline.py
+++ airbyte_dagster/dagster_pipeline.py
@@ -52,6 +52,26 @@ dbt = dbt_cli_resource.configured({
     "profiles_dir": "/home/shuhailey/lzs-ppa/ppa-dbt-dagster/ppa_dbt",
 })
 
+# Custom op to create the table if it doesn't exist
+@op(required_resource_keys={"sqlserver_db"})
+def create_table_if_not_exists(context):
+    sqlserver_conn = context.resources.sqlserver_db
+    cursor = sqlserver_conn.cursor()
+
+    create_table_query = """
+    IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='asnaf_sample_transformed' AND xtype='U')
+    CREATE TABLE dbo.asnaf_sample_transformed (
+        AsnafID VARCHAR(500),  -- Increased length
+        AsnafName VARCHAR(500),  -- Increased length
+        Emel VARCHAR(500),  -- Increased length
+        Age INT
+    );
+    """
+    cursor.execute(create_table_query)
+    sqlserver_conn.commit()
+    cursor.close()
+
+
 # Custom op to transfer data from PostgreSQL to SQL Server
 @op(required_resource_keys={"postgres_db", "sqlserver_db"})
 def transfer_data_to_sqlserver(context):
@@ -68,15 +88,22 @@ def transfer_data_to_sqlserver(context):
             (_airbyte_data->>'vwlzs_Email')::text as Emel,
             (_airbyte_data->>'vwlzs_Age')::int as Age
         FROM airbyte_internal.dbo_raw__stream_vwlzs_asnaf
+        LIMIT 10
     """)
     rows = cursor_pg.fetchall()
 
     # Insert data into SQL Server
     for row in rows:
+        truncated_row = (
+            row[0][:500],  # Truncate AsnafID to 500 characters
+            row[1][:500],  # Truncate AsnafName to 500 characters
+            row[2][:500],  # Truncate Emel to 500 characters
+            row[3],        # Age doesn't need truncation
+        )
         cursor_sql.execute("""
             INSERT INTO dbo.asnaf_transformed (AsnafID, AsnafName, Emel, Age)
             VALUES (?, ?, ?, ?)
-        """, row)
+        """, truncated_row)
 
     sqlserver_conn.commit()
     cursor_pg.close()
@@ -86,6 +113,7 @@ def transfer_data_to_sqlserver(context):
 def ppa_data_pipeline():
     #sync_ppa_asnaf()
     dbt_run_op()
+    create_table_if_not_exists()
     transfer_data_to_sqlserver()
 
 @repository
